82504221c3e9db044391219128275f5f35f07fbf
Babur Saburov <b.saburov@yt.uz>
2023-07-14 16:17:53 +0500

    init empty vite antd project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { DatePicker } from "antd";
+import Som from "@/pages/som";
+
+const App = () => {
+  return (
+    <>
+      <DatePicker />
+      <Som />
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/som.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function som() {
+  return <div>som</div>;
+}
+
+export default som;
