4173fcef5bb21b6cfc9dceb82885fe3871e7791b
Babur Saburov <b.saburov@yt.uz>
2023-11-14 18:14:41 +0500

    update company admin page layout

--- src/pages/admin/AdminLayout.tsx
+++ src/pages/admin/AdminLayout.tsx
@@ -1,136 +1,39 @@
-import { Dropdown, MenuProps, Select } from 'antd';
-import { Building, List, LogOut, Users } from 'lucide-react';
+import { Building, List, Users } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
-import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
-import Logo from '@/Logo';
+import { Outlet } from 'react-router-dom';
 import { TAdminPages } from '@/App';
-import clsx from 'clsx';
-import useAuthUser from '@/auth/useAuthUser';
-import { get } from 'lodash';
-import { TLanguages } from '@/auth/i18n';
+import Layout from '@/pages/admin/Layout';
 
 function AdminLayout() {
 	const { t, i18n } = useTranslation();
-	const navigate = useNavigate();
-	const { page } = useParams();
 
 	const items: {
-		key: TAdminPages;
+		key: `/admin/${TAdminPages}`;
 		icon: React.ReactNode;
 		label: string;
 	}[] = [
 		{
-			key: 'business-analytics',
+			key: '/admin/business-analytics',
 			icon: <Building strokeWidth={1.5} className='!h-5' />,
 			label: t(`Бизнес аналитика`),
 		},
 		{
-			key: 'clients',
+			key: '/admin/clients',
 			icon: <Users strokeWidth={1.5} className='!h-5' />,
 			label: t(`Реестр клиентов`),
 		},
 		{
-			key: 'companies',
+			key: '/admin/companies',
 			icon: <List strokeWidth={1.5} className='!h-5' />,
 			label: t(`Реестр кредитующих организации`),
 		},
 	];
 
-	const user = useAuthUser();
-
-	const name = get(user, 'data.data.data.fullName', null);
-	const companyName = get(user, 'data.data.data.company.name', null);
-	const rolesName = get(user, 'data.data.data.roles.0.name', null);
-
-	const changeLanguageHandler = (lang: TLanguages) => {
-		i18n.changeLanguage(lang);
-	};
-
-	const navItems: MenuProps['items'] = [
-		{
-			label: (
-				<Link className='!text-red-500' to={`/logout`}>
-					{t('Chiqish')}
-				</Link>
-			),
-			icon: <LogOut size={18} strokeWidth={1.5} className='text-red-500' />,
-			key: '1',
-		},
-	];
-
 	return (
 		<>
-			<div className='flex h-screen'>
-				<div className='w-[320px] border-r border-gray-100 flex-shrink-0'>
-					<div className='h-full flex flex-col justify-between'>
-						<div className='!px-3'>
-							<div className='!py-4 !pt-5'>
-								<Logo className='!h-6' />
-							</div>
-
-							{items.map((item, idx) => {
-								return (
-									<div
-										key={idx}
-										onClick={() => navigate(`/admin/${item.key}`)}
-										className={clsx({
-											'flex items-center !gap-3 bg-white text-gray-700 !py-3 !px-3 rounded-lg active:scale-95 duration-200 cursor-pointer select-none':
-												true,
-											'!bg-[#12b855]/10 !text-[#12b855]': window.location.pathname.includes(item.key),
-										})}
-									>
-										{item.icon}
-										<p className='!my-0 text-sm'>{item.label}</p>
-									</div>
-								);
-							})}
-						</div>
-
-						<div className='space-y-5'>
-							<div className='px-3'>
-								<Select
-									className='w-full'
-									defaultValue={'ru'}
-									onSelect={(e) => {
-										changeLanguageHandler(e as TLanguages);
-									}}
-									options={
-										[
-											{
-												label: 'Русский',
-												value: 'ru',
-											},
-											{
-												label: 'Ўзбекча',
-												value: 'uz_kyrl',
-											},
-											{
-												label: "O'zbekcha",
-												value: 'uz_latn',
-											},
-										] satisfies { label: React.ReactNode; value: TLanguages }[]
-									}
-								/>
-							</div>
-
-							<Dropdown menu={{ items: navItems }} trigger={['click']} className='hover:bg-gray-100 px-3 py-3'>
-								<div className='flex items-center !gap-3 cursor-pointer'>
-									<img
-										className='rounded-full w-8 h-8'
-										src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-										alt='image'
-									/>
-									{user.data ? <div className='font-medium text-sm'>{companyName}</div> : null}
-								</div>
-							</Dropdown>
-						</div>
-					</div>
-				</div>
-
-				<div className='flex-grow py-5 px-5 bg-gray-50/20'>
-					<Outlet />
-				</div>
-			</div>
+			<Layout items={items}>
+				<Outlet />
+			</Layout>
 		</>
 	);
 }
--- src/pages/company/CompanyLayout.tsx
+++ src/pages/company/CompanyLayout.tsx
@@ -1,22 +1,18 @@
-import { Layout, Menu, MenuProps, theme } from 'antd';
-import { BarChart2, Building, LineChart, Users,BookIcon, ListIcon } from 'lucide-react';
+import { BarChart2, Building, LineChart, Users, BookIcon, ListIcon } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
-import { Outlet, useNavigate } from 'react-router-dom';
-import Header from '@/pages/header/Header.tsx';
-import Logo from '@/Logo';
+import { Outlet } from 'react-router-dom';
 import { TCompanyPages } from '@/App';
 import React from 'react';
+import Layout from '@/pages/admin/Layout';
 
 function CompanyLayout() {
 	const { t, i18n } = useTranslation();
-	const navigate = useNavigate();
 
 	const items: {
 		key: TCompanyPages;
 		icon: React.ReactNode;
 		label: string;
 	}[] = [
-
 		{
 			key: 'analytics',
 			icon: <LineChart strokeWidth={1.5} className='!h-5' />,
@@ -47,40 +43,14 @@ function CompanyLayout() {
 			icon: <ListIcon strokeWidth={1.5} className='!h-5' />,
 			label: t(`Список заявлений`),
 		},
-
 	];
 
-	const {
-		token: { colorBgContainer },
-	} = theme.useToken();
-
 	return (
-		<div>
-			<Layout>
-				<Layout.Sider
-					width={240}
-					style={{
-						background: colorBgContainer,
-					}}
-				>
-					<div className='flex items-center justify-center py-3'>
-						<Logo className='!h-10' />
-					</div>
-
-					<Menu
-						theme='light'
-						mode='inline'
-						items={items}
-						selectedKeys={[]}
-						onClick={(e) => navigate(`${e.key}`)}
-					/>
-				</Layout.Sider>
-				<Layout style={{ padding: '0 24px 24px' }} className='bg-white'>
-					<Header />
-					<Outlet />
-				</Layout>
+		<>
+			<Layout items={items}>
+				<Outlet />
 			</Layout>
-		</div>
+		</>
 	);
 }
 
