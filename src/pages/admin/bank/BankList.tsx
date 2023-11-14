4f8035b213b7d3abcc37c941ff36adce77bc1b90
Babur Saburov <b.saburov@yt.uz>
2023-11-14 18:05:49 +0500

    update formatting

--- src/pages/admin/bank/BankList.tsx
+++ src/pages/admin/bank/BankList.tsx
@@ -1,95 +1,102 @@
-import {req} from "@/services/api.ts";
-import {Text} from "@geist-ui/core";
-import {useQuery} from "@tanstack/react-query";
-import {Button, Layout, Menu, Segmented, Select, Table, theme} from "antd";
-import {ColumnsType} from "antd/es/table";
-import {get} from "lodash";
-import {useTranslation} from "react-i18next";
-import AddBankModal from "@/pages/admin/bank/AddBankModal.tsx";
-import moment from "moment";
+import { req } from '@/services/api.ts';
+import { Text } from '@geist-ui/core';
+import { useQuery } from '@tanstack/react-query';
+import { Table } from 'antd';
+import { ColumnsType } from 'antd/es/table';
+import { get } from 'lodash';
+import { useTranslation } from 'react-i18next';
+import AddBankModal from '@/pages/admin/bank/AddBankModal.tsx';
+import moment from 'moment';
+import { formatNumber } from '@/auth/Scoring';
 
 interface IBank {
-    id: string | number;
-    bankName: string | number;
-    bankTin: string | number;
-    bankShare: string | number;
-    bankInsurance: string | number;
-    operatorShare: string | number;
-    createdAt: string | number;
+	id: string | number;
+	bankName: string | number;
+	bankTin: string | number;
+	bankShare: string | number;
+	bankInsurance: string | number;
+	operatorShare: string | number;
+	createdAt: string | number;
 }
 
 export default function BankList() {
-    const {t} = useTranslation();
+	const { t } = useTranslation();
 
-    const queryBanks = useQuery({
-        queryKey: ["queryBanks"],
-        queryFn: () => {
-            return req({
-                method: "GET",
-                url: `/admin/get-banks`,
-                params: {
-                    //
-                },
-            });
-        },
-    });
-    const data = get(queryBanks, "data.data.data", []) as IBank[];
-    const total = get(queryBanks, "data.data.data.totalElements", 0) as number;
+	const queryBanks = useQuery({
+		queryKey: ['queryBanks'],
+		queryFn: () => {
+			return req({
+				method: 'GET',
+				url: `/admin/get-banks`,
+				params: {
+					//
+				},
+			});
+		},
+	});
+	const data = get(queryBanks, 'data.data.data', []) as IBank[];
+	const total = get(queryBanks, 'data.data.data.totalElements', 0) as number;
 
-    const columns: ColumnsType<IBank> = [
-        {
-            title: "",
-            dataIndex: "NONE",
-            render(value, record, index) {
-                return <>{1 + index}</>;
-            },
-        },
-        {
-            title: t("bankTin"),
-            dataIndex: "bankTin",
-            align: 'center',
-        },
-        {
-            title: t("bankName"),
-            dataIndex: "bankName",
-            align: 'center',
-        },
-        {
-            title: t("bankShare"),
-            dataIndex: "bankShare",
-            align: 'center',
-        },
-        {
-            title: t("bankInsurance"),
-            dataIndex: "bankInsurance",
-            align: 'center',
-        },
-        {
-            title: t("operatorShare"),
-            dataIndex: "operatorShare",
-            align: 'center',
-        },
-        {
-            title: t("createdAt"),
-            dataIndex: "createdAt",
-            align: 'center',
-            render(value, record, index) {
-                return (
-                    moment(value).format("DD.MM.YYYY")
-                );
-            },
-        },
-    ];
-    return (
-        <>
-            <Text h3>{t("Реестр кредитующих организации")}</Text>
-            <div className="h-[20px]"/>
-            <div className="w-full flex items-center justify-end">
-                <AddBankModal onAdd={() => queryBanks.refetch()}/>
-            </div>
-            <div className="h-[20px]"/>
-            <Table pagination={false} dataSource={data} columns={columns}/>
-
-        </>
-    );
+	const columns: ColumnsType<IBank> = [
+		{
+			title: '',
+			dataIndex: 'NONE',
+			render(value, record, index) {
+				return <>{1 + index}</>;
+			},
+		},
+		{
+			title: t('bankTin'),
+			dataIndex: 'bankTin',
+			align: 'center',
+		},
+		{
+			title: t('bankName'),
+			dataIndex: 'bankName',
+			align: 'center',
+		},
+		{
+			title: t('bankShare'),
+			dataIndex: 'bankShare',
+			align: 'center',
+			render(value, record, index) {
+				return formatNumber(value);
+			},
+		},
+		{
+			title: t('bankInsurance'),
+			dataIndex: 'bankInsurance',
+			align: 'center',
+			render(value, record, index) {
+				return formatNumber(value);
+			},
+		},
+		{
+			title: t('operatorShare'),
+			dataIndex: 'operatorShare',
+			align: 'center',
+			render(value, record, index) {
+				return formatNumber(value);
+			},
+		},
+		{
+			title: t('createdAt'),
+			dataIndex: 'createdAt',
+			align: 'center',
+			render(value, record, index) {
+				return moment(value).format('DD.MM.YYYY');
+			},
+		},
+	];
+	return (
+		<>
+			<Text h3>{t('Реестр кредитующих организации')}</Text>
+			<div className='h-[20px]' />
+			<div className='w-full flex items-center justify-end'>
+				<AddBankModal onAdd={() => queryBanks.refetch()} />
+			</div>
+			<div className='h-[20px]' />
+			<Table pagination={false} dataSource={data} columns={columns} />
+		</>
+	);
 }
--- src/pages/admin/business-analytics/index.tsx
+++ src/pages/admin/business-analytics/index.tsx
@@ -1,97 +1,108 @@
-import Header from "@/pages/header/Header.tsx";
-import {req} from "@/services/api.ts";
-import {Text} from "@geist-ui/core";
-import {useQuery} from "@tanstack/react-query";
-import {Segmented, Table, Tabs} from "antd";
-import {ColumnsType} from "antd/es/table";
-import {get} from "lodash";
-import React, {useState} from "react";
-import {useTranslation} from "react-i18next";
+import { formatNumber } from '@/auth/Scoring';
+import Header from '@/pages/header/Header.tsx';
+import { req } from '@/services/api.ts';
+import { Text } from '@geist-ui/core';
+import { useQuery } from '@tanstack/react-query';
+import { Segmented, Table, Tabs } from 'antd';
+import { ColumnsType } from 'antd/es/table';
+import { get } from 'lodash';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 function BusinessAnalytics() {
-    const [filter, setFilter] = useState({
-        tab: "all",
-    });
-    const queryBusinessAnalytics = useQuery({
-        queryKey: ["queryBusinessAnalytics", filter.tab],
-        queryFn: () => {
-            return req({
-                method: "GET",
-                url: `/stat/get-analytic-stat-monthly`,
-                params: {
-                    //
-                },
-            });
-        },
-    });
-    const data = get(queryBusinessAnalytics, "data.data.data", []);
-    const {t, i18n} = useTranslation();
-    const columns: ColumnsType<any> = [
-        {
-            title: "",
-            dataIndex: "",
-            render(value, record, index) {
-                return <>{1 + index}</>;
-            },
-        },
-        {
-            title: t("Савдо белгиси (бренд)"),
-            dataIndex: "companyBrand",
-        },
-        {
-            title: t("Харидорлар сони"),
-            dataIndex: "consumerCnt",
-        },
-        {
-            title: t("Аризалар сони"),
-            dataIndex: "applicationCnt",
-        },
-        {
-            title: t("Маҳсулотлар сони"),
-            dataIndex: "productCnt",
-        },
-        {
-            title: t("Харид суммаси"),
-            dataIndex: "purchaseSum",
-        },
-        {
-            title: t("Хизмат кўрсатувчи банк"),
-            dataIndex: "bankName",
-        },
-        {
-            title: t("Оператор улуши"),
-            dataIndex: "operatorShare",
-        },
-        {
-            title: t("Оператор фойдаси"),
-            dataIndex: "operatorProfit",
-        },
-    ];
+	const [filter, setFilter] = useState({
+		tab: 'all',
+	});
+	const queryBusinessAnalytics = useQuery({
+		queryKey: ['queryBusinessAnalytics', filter.tab],
+		queryFn: () => {
+			return req({
+				method: 'GET',
+				url: `/stat/get-analytic-stat-monthly`,
+				params: {
+					//
+				},
+			});
+		},
+	});
+	const data = get(queryBusinessAnalytics, 'data.data.data', []);
+	const { t, i18n } = useTranslation();
+	const columns: ColumnsType<any> = [
+		{
+			title: '',
+			dataIndex: '',
+			render(value, record, index) {
+				return <>{1 + index}</>;
+			},
+		},
+		{
+			title: t('Савдо белгиси (бренд)'),
+			dataIndex: 'companyBrand',
+		},
+		{
+			title: t('Харидорлар сони'),
+			dataIndex: 'consumerCnt',
+		},
+		{
+			title: t('Аризалар сони'),
+			dataIndex: 'applicationCnt',
+		},
+		{
+			title: t('Маҳсулотлар сони'),
+			dataIndex: 'productCnt',
+		},
+		{
+			title: t('Харид суммаси'),
+			dataIndex: 'purchaseSum',
+			render(value, record, index) {
+				return formatNumber(value);
+			},
+		},
+		{
+			title: t('Хизмат кўрсатувчи банк'),
+			dataIndex: 'bankName',
+		},
+		{
+			title: t('Оператор улуши'),
+			dataIndex: 'operatorShare',
+			render(value, record, index) {
+				return formatNumber(value);
+			},
+		},
+		{
+			title: t('Оператор фойдаси'),
+			dataIndex: 'operatorProfit',
+			render(value, record, index) {
+				return formatNumber(value);
+			},
+		},
+	];
 
-    return (
-        <>
-            <Text h3>Бизнес аналитика</Text>
-            <div className="h-[20px]"/>
-            <div className="w-full flex items-center justify-start">
-                <Segmented
-                onChange={(tab) => setFilter({...filter, tab: tab as string})}
-                value={filter.tab}
-                options={[
-                    {
-                        label: t("Фаолият бошидан"),
-                        value: "all",
-                    },
-                    {
-                        label: t("Жорий ой"),
-                        value: "current_month",
-                    },
-                ]}
-            /></div>
+	return (
+		<>
+			<Text h3>Бизнес аналитика</Text>
+			<div className='h-[20px]' />
+			<div className='w-full flex items-center justify-start'>
+				<Segmented
+					onChange={(tab) => setFilter({ ...filter, tab: tab as string })}
+					value={filter.tab}
+					options={[
+						{
+							label: t('Фаолият бошидан'),
+							value: 'all',
+						},
+						{
+							label: t('Жорий ой'),
+							value: 'current_month',
+						},
+					]}
+				/>
+			</div>
 
-            <div className="h-[20px]"/>
-            <Table pagination={false} dataSource={data} columns={columns}/>
-        </>
-    );
+			<div className='h-[20px]' />
+			<Table pagination={false} dataSource={data} columns={columns} />
+		</>
+	);
 }
 
 export default BusinessAnalytics;
